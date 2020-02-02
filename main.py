65a711748e3c1d971bd6ba9290c4c929f95d3f48
P4GAN <55480921+P4GAN@users.noreply.github.com>
2020-02-02 21:21:02 +1100

    look is var

--- main.py
+++ main.py
@@ -7,15 +7,16 @@ d="You are in the depths of the"+t
 p=print
 a=1
 l="You are standing at the entrance to a long"+t
-n=", look\n> "
+x="look"
+n=", ",x,"\n> "
 z=input
 while a!=f:
   if r<1:
     a=z(c+f+n)
-    if a=="look":p(l)
+    if a==x:p(l)
     elif a==f:p(d);r=1
   if r>0:
     a=z(c+f+f", {b}"+n)
     if a==b:p(l);r=0
-    elif a=="look":p(d)
+    elif a==x:p(d)
 p("You have escaped the"+t,"You win!")
