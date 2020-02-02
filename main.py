bb5660d96378a9ad1e20a3d04e3a39b0a927301b
P4GAN <55480921+P4GAN@users.noreply.github.com>
2020-02-02 21:18:15 +1100

    fixed google docs speech marks and made input var

--- main.py
+++ main.py
@@ -2,19 +2,20 @@ r=0
 f="forward"
 b="backward"
 c="actions available: "
-t=” tunnel.”
+t=" tunnel."
 d="You are in the depths of the"+t
 p=print
 a=1
 l="You are standing at the entrance to a long"+t
 n=", look\n> "
+z=input
 while a!=f:
   if r<1:
-    a=input(c+f+n)
+    a=z(c+f+n)
     if a=="look":p(l)
     elif a==f:p(d);r=1
   if r>0:
-    a=input(c+f+f", {b}"+n)
+    a=z(c+f+f", {b}"+n)
     if a==b:p(l);r=0
     elif a=="look":p(d)
-p("You have escaped the”+t,”You win!")
+p("You have escaped the"+t,"You win!")
