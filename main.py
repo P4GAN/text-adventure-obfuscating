02fc516222660b598d62c6c118208a3cef543ba1
Brendan <48776053+I-Agreed@users.noreply.github.com>
2020-02-02 20:35:06 +1100

    changing string combination
    changed {}s to +

--- main.py
+++ main.py
@@ -8,11 +8,11 @@ a=1
 l="You are standing at the entrance to a long tunnel."
 while a!=f:
   if r<1:
-    a=input(f"{c}{f}, look\n> ")
+    a=input(c+f+", look\n> ")
     if a=="look":p(l)
     elif a==f:p(d);r=1
   if r==1:
-    a=input(f"{c}{f}, {b}, look\n> ")
+    a=input(c+f+", {b}, look\n> ")
     if a==b:p(l);r=0
     elif a=="look":p(d)
 p("You have escaped the tunnel. You win!")
